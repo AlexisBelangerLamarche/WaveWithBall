a5a4e0f19d0fd9066da473776713174de1d6968f
AlexisBelangerLamarche <belangeralexis2@gmail.com>
2022-05-10 11:05:38 -0400

    OutOfBounds catcher

---
+++ Assets/Scripts/TeleportScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportScript : MonoBehaviour
+{
+
+    public GameObject teleportEnd;
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        other.gameObject.transform.position = teleportEnd.transform.position;
+
+    }
+
+}
